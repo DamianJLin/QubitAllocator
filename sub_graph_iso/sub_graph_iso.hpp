9dc32b988c5d8e865fbe5644caf7e933da18ba38
Damian Lin <damianlin@iinet.net.au>
2022-01-26 22:09:12 +1100

    work in progress

--- sub_graph_iso/sub_graph_iso.hpp
+++ sub_graph_iso/sub_graph_iso.hpp
@@ -9,8 +9,19 @@
 	
 using namespace lemon;
 
-// graph = *import graph from wherever. For now:
-int subgraph_isom_finder() {
+// Function to be called in the body.
+// Signature map, subgraph -> map.
+ListGraph::NodeMap<int> match_state(ListGraph::NodeMap<int> map_in, subgraph) {
+	
+	bool flag_mapping_complete = false;
+	for (ListGraph::NodeIt map_in(
+		
+	return 
+}
+
+
+// 'Main body function'.
+int VF2() {
 
 	// For now...
 	ListGraph g; //    1           4
@@ -33,19 +44,23 @@ int subgraph_isom_finder() {
 	s.addEdge(g.nodeFromId(0), g.nodeFromId(1));
 	s.addEdge(g.nodeFromId(1), g.nodeFromId(2));
 	s.addEdge(g.nodeFromId(2), g.nodeFromId(3));
-	s.addEdge(g.nodeFromId(3), g.nodeFromId(0));
 
 	ListGraph::NodeMap<int> map(s); // Map from s to (index of) g. Beware the asymmetry:
-									// map[ . ]: {slet of Nodes} --> {set of ints}
+									// map[ . ]: {set of nodes of S} --> {set of ints}
 	// Set map to -1 initially.
 	for (ListGraph::NodeIt n(g); n != INVALID; ++n) { 
 		map[n] = -1;
 	}
 	std::cout << map[g.nodeFromId(0)] << std::endl;
 
+	
+	ListGraph::NodeMap<int> match() {
+
+	}	
 
 
 	return 0;
 }
+
 
 #endif
